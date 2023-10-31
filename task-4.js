d1e2a7ff40a09de895296ff814124033d1038025
Oleksii0210 <141589602+Oleksii0210@users.noreply.github.com>
2023-10-31 16:15:53 +0200

    new

--- task-4.js
+++ task-4.js
@@ -1,1 +1,17 @@
-console.log(444444444);
+"use strict";
+function getShippingCost(country){
+    const getShippingCost = `Shipping to ${country} will cost ${price} credits`;
+    return getShippingCost;
+}
+
+console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
+
+//China — 100 кредитів if price < 100
+//Chile — 250 кредитів
+//Australia — 170 кредитів
+//Jamaica — 120 кредитів
