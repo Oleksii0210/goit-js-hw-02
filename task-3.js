9334f354c39e8b1354f4a83f4bfccdfc1f847e72
Oleksii0210 <141589602+Oleksii0210@users.noreply.github.com>
2023-12-08 14:56:38 +0200

    new

--- task-3.js
+++ task-3.js
@@ -1,10 +1,10 @@
 "use strict";
 function checkForSpam(message){
     if (message.includes("spam") || message.includes("sale")){
-        return `false`;
+        return `true`;
     }
     else {
-        return `true`;
+        return `false`;
     }
 
 }
@@ -15,4 +15,15 @@ console.log(checkForSpam("Get best sale offers now!")); // true
 console.log(checkForSpam("Amazing SalE, only tonight!")); // true
 console.log(checkForSpam("Trust me, this is not a spam message")); // true
 console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // 
+
+
+function calculateTotal(number) {
+    let counter = 0;
+    let sun = 0;
+    while (counter < number) {
+      counter  += 1;
+      sun = sun + counter;
+    }
+    return sun;
+  }
